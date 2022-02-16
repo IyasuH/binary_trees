1997050c7ae02b9449220e6655b858a431953440
IyasuH <eyasuhailegbr@gmail.com>
2022-02-16 08:58:15 -0800

    update

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -13,12 +13,12 @@ if (tree != NULL)
 if (tree->left)
 {
 binary_tree_delete(tree->left);
-free (tree->left);
+free(tree->left);
 }
 if (tree->right)
 {
 binary_tree_delete(tree->right);
-free (tree->right);
+free(tree->right);
 }
 }
 }
